2f49f6fa6c2ea0b7812f98cfb76eb5e05bd9a96b
Christian Liebel <christian.liebel@thinktecture.com>
2024-10-06 08:43:21 -0400

    Lab #10

--- tools/my-plugin/src/generators/my-step/generator.ts
+++ tools/my-plugin/src/generators/my-step/generator.ts
@@ -1,6 +1,8 @@
-import { Tree } from '@nx/devkit';
-import { dasherize } from '@nx/devkit/src/utils/string-utils';
+import { applyChangesToString, ChangeType, joinPathFragments, readProjectConfiguration, Tree } from '@nx/devkit';
+import { classify, dasherize } from '@nx/devkit/src/utils/string-utils';
 import { wrapAngularDevkitSchematic } from 'nx/src/adapter/ngcli-adapter';
+import { Identifier, isIdentifier, isVariableStatement } from 'typescript';
+import { getSourceFile } from '../utils/get-source-file';
 import { MyStepGeneratorSchema } from './schema';
 
 export async function myStepGenerator(
@@ -15,6 +17,24 @@ export async function myStepGenerator(
     name: dasherize(options.name),
     project: options.project,
   });
+
+  const projectDir = readProjectConfiguration(tree, options.project).root;
+  const workflowFileName = joinPathFragments(projectDir, 'src', 'app', 'app.workflow.ts');
+  const sourceFile = getSourceFile(tree, workflowFileName);
+
+  const stepsDeclaration = sourceFile.statements
+    .filter(isVariableStatement)
+    .map(s => s.declarationList.declarations[0])
+    .filter(d => isIdentifier(d.name))
+    .find(d => (d.name as Identifier).escapedText === 'loginSteps');
+
+  const stepToAdd = `"${classify(options.name)}",`;
+  const newContents = applyChangesToString(sourceFile.text, [{
+    type: ChangeType.Insert,
+    index: stepsDeclaration.end - 1,
+    text: stepToAdd
+  }]);
+  tree.write(workflowFileName, newContents);
 }
 
 export default myStepGenerator;
